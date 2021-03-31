50733054fbb822af22c840e35fc3e14d7d6f5575
noliosahrak <wsburghardt@gmail.com>
2021-03-31 17:10:59 +0200

    raise Exception when empty input

---
+++ NameInverter.py
@@ -0,0 +1,7 @@
+class NameInverter:
+
+    def invert(self, name):
+        if name == "":
+            raise ValueError
+        else:
+            return name
--- test_NameInventer.py
+++ test_NameInventer.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from assertpy import assert_that
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from NameInverter import NameInverter
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class TestNameInverter:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def test_should_raise_exception_when_null_provided(self):
+        name_inverter = NameInverter()
+        assert_that(name_inverter.invert).raises(ValueError).when_called_with("")
